c5589a655165e15fb3364ad998ae145f5c6aebc0
ismdibrahim <ibrahiminfo77@gmail.com>
2023-05-18 18:17:53 +0600

    construct, destructor, abstract class

---
+++ oop_w3school/abstract_class.php
@@ -0,0 +1,7 @@
+<?php 
+abstract class ParentClass{
+    abstract public function SomeMethod1();
+    abstract public function SomeMethod2($name, $color);
+    abstract public function SomeMethod3() : string;
+}
+?>
